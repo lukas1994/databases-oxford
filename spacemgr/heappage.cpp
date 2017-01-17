6f0adc5f8b098ce09fa0b3be26efd21a1bb9d629
Lukas Kobis <ug13lk@tr01.tcs.cs.ox.ac.uk>
2017-01-17 14:41:49 +0000

    init

---
+++ spacemgr/heappage.cpp
@@ -0,0 +1,185 @@
+#include <iostream>
+#include <stdlib.h>
+#include <memory.h>
+
+#include "../include/heappage.h"
+#include "../include/heapfile.h"
+#include "../include/bufmgr.h"
+#include "../include/db.h"
+
+using namespace std;
+
+//------------------------------------------------------------------
+// Constructor of HeapPage
+//
+// Input     : Page ID
+// Output    : None
+//------------------------------------------------------------------
+
+void HeapPage::Init(PageID pageNo)
+{
+	//TODO: add your code here
+}
+
+void HeapPage::SetNextPage(PageID pageNo)
+{
+	//TODO: add your code here
+}
+
+void HeapPage::SetPrevPage(PageID pageNo)
+{
+	//TODO: add your code here
+}
+
+PageID HeapPage::GetNextPage()
+{
+	//TODO: add your code here
+  return NULL;
+}
+
+PageID HeapPage::GetPrevPage()
+{
+	//TODO: add your code here
+  return NULL;
+}
+
+
+//------------------------------------------------------------------
+// HeapPage::InsertRecord
+//
+// Input     : Pointer to the record and the record's length 
+// Output    : Record ID of the record inserted.
+// Purpose   : Insert a record into the page
+// Return    : OK if everything went OK, DONE if sufficient space 
+//             does not exist
+//------------------------------------------------------------------
+
+Status HeapPage::InsertRecord(char *recPtr, int length, RecordID& rid)
+{
+	//TODO: add your code here
+}
+
+
+//------------------------------------------------------------------
+// HeapPage::DeleteRecord 
+//
+// Input    : Record ID
+// Output   : None
+// Purpose  : Delete a record from the page
+// Return   : OK if successful, FAIL otherwise  
+//------------------------------------------------------------------ 
+
+Status HeapPage::DeleteRecord(const RecordID& rid)
+{
+	//TODO: add your code here
+	return OK;
+}
+
+
+//------------------------------------------------------------------
+// HeapPage::FirstRecord
+//
+// Input    : None
+// Output   : record id of the first record on a page
+// Purpose  : To find the first record on a page
+// Return   : OK if successful, DONE otherwise
+//------------------------------------------------------------------
+
+Status HeapPage::FirstRecord(RecordID& rid)
+{
+	//TODO: add your code here
+	return DONE;
+}
+
+
+//------------------------------------------------------------------
+// HeapPage::NextRecord
+//
+// Input    : ID of the current record
+// Output   : ID of the next record
+// Return   : Return DONE if no more records exist on the page; 
+//            otherwise OK
+//------------------------------------------------------------------
+
+Status HeapPage::NextRecord (RecordID curRid, RecordID& nextRid)
+{
+	//TODO: add your code here
+	return DONE;
+}
+
+
+//------------------------------------------------------------------
+// HeapPage::GetRecord
+//
+// Input    : Record ID
+// Output   : Records length and a copy of the record itself
+// Purpose  : To retrieve a _copy_ of a record with ID rid from a page
+// Return   : OK if successful, FAIL otherwise
+//------------------------------------------------------------------
+
+Status HeapPage::GetRecord(RecordID rid, char *recPtr, int& length)
+{
+	//TODO: add your code here
+    return OK;
+}
+
+
+//------------------------------------------------------------------
+// HeapPage::ReturnRecord
+//
+// Input    : Record ID
+// Output   : pointer to the record, record's length
+// Purpose  : To output a _pointer_ to the record
+// Return   : OK if successful, FAIL otherwise
+//------------------------------------------------------------------
+
+Status HeapPage::ReturnRecord(RecordID rid, char*& recPtr, int& length)
+{
+	//TODO: add your code here
+    return OK;
+}
+
+
+//------------------------------------------------------------------
+// HeapPage::AvailableSpace
+//
+// Input    : None
+// Output   : None
+// Purpose  : To return the amount of available space
+// Return   : The amount of available space on the heap file page.
+//------------------------------------------------------------------
+
+int HeapPage::AvailableSpace(void)
+{
+	//TODO: add your code here
+	return 0;
+}
+
+
+//------------------------------------------------------------------
+// HeapPage::IsEmpty
+// 
+// Input    : None
+// Output   : None
+// Purpose  : Check if there is any record in the page.
+// Return   : true if the HeapPage is empty, and false otherwise.
+//------------------------------------------------------------------
+
+bool HeapPage::IsEmpty(void)
+{
+	//TODO: add your code here
+	return true;
+}
+
+
+void HeapPage::CompactSlotDir()
+{
+  // Complete this method to get the S+ mark.
+  // This method is not required for an S mark.
+}
+
+int HeapPage::GetNumOfRecords()
+{
+	//TODO: add your code here
+  return 0;
+}
